20e7293d4acf4a78bffe2dcdeeb485126df5f3cc
Hilton Meyer <development@hiltonmeyer.com>
2022-06-20 22:34:02 +0300

    [setup](css): add tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,15 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
+// import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <h1 class="text-4xl text-center">Dew Point Calculator</h1>
   </header>
-
-  <RouterView />
 </template>
 
 <style>
-@import "@/assets/base.css";
+@import '@/assets/tailwind.css';
 
 #app {
   max-width: 1280px;
